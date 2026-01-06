34c1e3f7ea5a0f4431fe553b3187ab406f2f6627
August <139867239+prashannasthapit@users.noreply.github.com>
2026-01-06 08:51:44 +0545

    remove lab10 files

--- Maui/Lab10/Product.cs
+++
@@ -1,8 +0,0 @@
-namespace Maui.Lab10;
-
-public class Product
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; } = null!;
-    public string Description { get; set; } = null!;
-}
